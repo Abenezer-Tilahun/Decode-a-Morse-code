7ef57d37e0f88f9fc0597d30518804c795293d47
daudi13 <davouma13@gmail.com>
2022-05-11 17:36:00 +0300

    Add solution

--- morse-code.rb
+++ morse-code.rb
@@ -1,13 +1,12 @@
 def decode_char(character)
   morse = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', 
-    '-..' => 'D', '.' => 'E', '..-.' => 'F',
-    '--.' => 'G', '....' => 'H', '..' => 'I',
-    '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-    '--' => 'M', '-.' => 'N', '---' => 'O', 
-    '.--.' => 'P', '--.-' => 'Q', '-.-' => 'R',
-    '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V',
-    '.--' => 'W', '-..-' => 'U', '-.--' => 'Y', '--..' => 'Z'
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
   }
 
   morse[character]
@@ -17,10 +16,16 @@ end
 puts decode_char('.-')
 
 def decode_word(word)
-  letters=word.split
-  decoded=''
-  letters.each {|letter| decoded += decode_char(letter)}
-  decoded+=' '
+  letters = word.split
+  letters.map { |letter| decode_char(letter)}.join
 end
 
 puts decode_word('.--- .--.')
+
+def decode(code)
+  code.split('   ').map { |word| decode_word(word) }.join(' ')
+
+end
+
+puts decode("-- -.--   -. .- -- .")
+puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
